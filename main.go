2048c521ce28a43e20677db18d9a928b705b15a6
kinobi <lionel@kinobi.pro>
2017-12-23 17:52:26 +0100

    Fix Consumer Key check too early

--- main.go
+++ main.go
@@ -5,20 +5,25 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/kinobi/ppocket/pocket"
 )
 
-const urlCallback = "https://ppocket.herokuapp.com/"
+const (
+	urlCallback   = "https://ppocket.herokuapp.com/"
+	wordPerMinute = 140
+)
 
 func main() {
 	ppocketConsumerKey := flag.String("k", os.Getenv("PPOCKET_API_CONSUMER_KEY"), "Consumer key")
+	ppocketUserAccessToken := flag.String("a", os.Getenv("PPOCKET_USER_ACCESS_TOKEN"), "User access token")
+	flag.Parse()
+
 	if *ppocketConsumerKey == "" {
 		log.Fatalln("Consumer key is missing")
 	}
-	ppocketUserAccessToken := flag.String("a", os.Getenv("PPOCKET_USER_ACCESS_TOKEN"), "User access token")
-	flag.Parse()
 
 	ppocketUsername := os.Getenv("PPOCKET_USERNAME")
 
@@ -33,10 +38,10 @@ func main() {
 	query := pocket.NewGetQuery(
 		pocket.WithState(pocket.QueryStateAll),
 		pocket.WithFavorite(pocket.QueryFavoriteOrNot),
-		pocket.WithTag("php"),
+		pocket.WithTag("golang"),
 		pocket.WithSort(pocket.QuerySortNewest),
 		pocket.WithSince(&since),
-		pocket.WithPagination(4, 4),
+		pocket.WithPagination(10, 0),
 	)
 
 	res, err := pocket.Get(*ppocketConsumerKey, *ppocketUserAccessToken, query)
@@ -48,6 +53,10 @@ func main() {
 		for tag := range item.Tags {
 			fmt.Println("\t- " + tag)
 		}
-		fmt.Printf("\t[%s words | status: %#v | favorite: %v]\n\n", item.WordCount, item.Status, item.Favorite)
+		wc, err := strconv.Atoi(item.WordCount)
+		if err != nil {
+			log.Println("Cannot convert word count to integer")
+		}
+		fmt.Printf("\t[%d min | status: %#v | favorite: %v]\n\n", wc/wordPerMinute, item.Status, item.Favorite)
 	}
 }
